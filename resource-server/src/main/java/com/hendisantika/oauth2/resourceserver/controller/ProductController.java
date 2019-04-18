ccb1ce454c08b9fd04b87382620769c7c9a87b3a
hendisantika <hendisantika@yahoo.co.id>
2019-04-19 06:39:16 +0700

    #13 - Add findByName method in ProductController  in resource-server

--- resource-server/src/main/java/com/hendisantika/oauth2/resourceserver/controller/ProductController.java
+++ resource-server/src/main/java/com/hendisantika/oauth2/resourceserver/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.hendisantika.oauth2.resourceserver.controller;
 import com.hendisantika.oauth2.resourceserver.domain.Product;
 import com.hendisantika.oauth2.resourceserver.repository.ProductRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +37,12 @@ public class ProductController {
     public Product getProduct(@PathVariable("id") Long id) {
         return productRepository.findById(id)
                 .orElse(null);
+    }
+
+    @GetMapping("/search/")
+    @PreAuthorize("hasRole('ROLE_PRODUCT_ADMIN')")
+    public Product findByName(@RequestParam("name") String name) {
+        return productRepository.findByNameLike("%" + name + "%");
     }
 
 }
