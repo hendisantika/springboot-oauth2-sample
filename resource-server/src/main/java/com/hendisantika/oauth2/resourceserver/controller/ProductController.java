2165f1e569ac0280cef3d5c027ff043c9524febc
hendisantika <hendisantika@yahoo.co.id>
2019-04-19 06:39:03 +0700

    #12 - Add getProduct method in ProductController  in resource-server

--- resource-server/src/main/java/com/hendisantika/oauth2/resourceserver/controller/ProductController.java
+++ resource-server/src/main/java/com/hendisantika/oauth2/resourceserver/controller/ProductController.java
@@ -2,7 +2,9 @@ package com.hendisantika.oauth2.resourceserver.controller;
 
 import com.hendisantika.oauth2.resourceserver.domain.Product;
 import com.hendisantika.oauth2.resourceserver.repository.ProductRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +35,11 @@ public class ProductController {
         return productRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_PRODUCT_ADMIN')")
+    public Product getProduct(@PathVariable("id") Long id) {
+        return productRepository.findById(id)
+                .orElse(null);
+    }
 
 }
