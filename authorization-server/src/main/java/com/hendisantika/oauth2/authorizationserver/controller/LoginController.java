4541359ce6a48886f9715518435fcd93fb9adb5e
hendisantika <hendisantika@yahoo.co.id>
2019-04-20 05:55:45 +0700

    #31 - Add root method in LoginController in authorization-server project

--- authorization-server/src/main/java/com/hendisantika/oauth2/authorizationserver/controller/LoginController.java
+++ authorization-server/src/main/java/com/hendisantika/oauth2/authorizationserver/controller/LoginController.java
@@ -1,8 +1,18 @@
 package com.hendisantika.oauth2.authorizationserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.provider.approval.Approval;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +27,23 @@ import org.springframework.stereotype.Controller;
 public class LoginController {
     @Autowired
     private JdbcClientDetailsService clientDetailsService;
+
+    @Autowired
+    private ApprovalStore approvalStore;
+
+    @RequestMapping("/")
+    public ModelAndView root(Map<String, Object> model, Principal principal) {
+
+
+        List<Approval> approvals = clientDetailsService.listClientDetails().stream()
+                .map(clientDetails -> approvalStore.getApprovals(principal.getName(), clientDetails.getClientId()))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+
+        model.put("approvals", approvals);
+        model.put("clientDetails", clientDetailsService.listClientDetails());
+        return new ModelAndView("index", model);
+
+    }
 }
